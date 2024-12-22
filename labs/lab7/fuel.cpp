a65014809e4f76a55d8d7c474a456daa02794d9a
Вадим Коханенко <vadik.kokhanenko@gmail.com>
2024-12-22 12:51:29 +0300

    Добавлен подсчёт памяти и временной сложности

--- labs/lab7/fuel.cpp
+++ labs/lab7/fuel.cpp
@@ -2,19 +2,19 @@
 #include <vector>
 #include <queue>
 
-int minRefuelStops(int target, int start_fuel, std::vector<std::vector<int>>& stations) {
-    int stations_count = stations.size();
-    int curr_fuel = start_fuel;
-    std::priority_queue<int> queue;
+int minRefuelStops(int target, int start_fuel, std::vector<std::vector<int>>& stations) { // + 4 + 4 + 24 байт
+    int stations_count = stations.size(); // +4 байта
+    int curr_fuel = start_fuel; // +4 байта
+    std::priority_queue<int> queue; // +32 байта
 
-    int i = 0;
-    int result = 0;
+    int i = 0; // +4 байта
+    int result = 0; // +4 байта
 
-    // начинаем ехать и едем, пока хватает топлива
+    // начинаем ехать и едем, пока хватает топлива. O(N), N - число остановок, тк каждый раз запускаем цикл работает до следующей остановки
     while(curr_fuel < target) {
-        // по пути добавляем в очередь станции, мимо которых проехали
+        // по пути добавляем в очередь станции, мимо которых проехали. O(M), M - колво станций, мимо которых проезжаем
         while(i < stations_count && curr_fuel >= stations[i][0]) {
-            queue.push(stations[i][1]);
+            queue.push(stations[i][1]); // +4 байта. O(logK), K - колво элементов в очереди,  K <= M
             i++;
         }
         
@@ -24,9 +24,9 @@ int minRefuelStops(int target, int start_fuel, std::vector<std::vector<int>>& st
         }
         
         // если встали и нужна заправка – выбираем станцию из посещенных ранее, где больше всего топлива
-        int new_fuel = queue.top();
+        int new_fuel = queue.top(); // +4 байта
         // удаляем из очереди, чтобы не учитывать в дальнешем пути
-        queue.pop();
+        queue.pop(); // O(logK)
         
         // обновляем топливо и число остановок
         curr_fuel += new_fuel;
@@ -34,3 +34,4 @@ int minRefuelStops(int target, int start_fuel, std::vector<std::vector<int>>& st
     }
     return result;   
 }
+// Итог по временной сложности: до O(N * M), N - число остановок, M - число станций
